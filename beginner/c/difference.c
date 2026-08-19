1b7bf0c59555678922991bb1e0ea841bd6d729f4
Idinaldo <idinaldooliveira220@gmail.com>
2026-08-19 03:14:19 -0300

    solution: add "difference" - 1007

---
+++ beginner/c/difference.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int a, b, c, d, difference;
+
+	scanf("%i%i%i%i", &a, &b, &c, &d);
+
+	difference = a * b - c * d;
+
+	printf("DIFERENCA = %i\n", difference);
+
+	return 0;
+}
